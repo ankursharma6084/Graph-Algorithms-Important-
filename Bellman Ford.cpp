f843bc3ecc89ec80d4f0b4b682b83a30c263607f
Ankur Kumar <61647822+ankursharma6084@users.noreply.github.com>
2022-07-11 23:40:43 +0530

    Update Bellman Ford.cpp

--- Bellman Ford.cpp
+++ Bellman Ford.cpp
@@ -30,3 +30,30 @@
         }
 	    return 0 ;
 	}
+
+
+int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    // Write your code here.
+    
+    vector<int> dist(n+1 , 1e9) ;
+    dist[src] = 0 ;
+    
+    // why n-1 bcz longest path is linked list graph 
+    // whose max length is n-1
+    // eg ->     1 -> 2 -> 3 -> 4 -> 5 (n = 5 , m = 5-1) ;
+    
+    for(int i=1; i<=n-1; i++)
+    {
+        for(auto &x: edges)
+        {
+            int from = x[0] , to = x[1] , wt = x[2] ;
+            if(dist[to] > dist[from] + wt)
+            {
+                dist[to] = dist[from] + wt ;
+            }
+        }
+    }
+    
+    return dist[dest] ;
+    
+}
