06bc64543e8d8d00bf1e8af72c4a125830b7157f
Ankur Kumar <61647822+ankursharma6084@users.noreply.github.com>
2022-07-11 23:38:41 +0530

    Create Bellman Ford.cpp

---
+++ Bellman Ford.cpp
@@ -0,0 +1,32 @@
+	int isNegativeWeightCycle(int n, vector<vector<int>>edges){
+	    // Code here
+	    
+	    vector<int> dist(n , 1e9) ;
+    
+       
+    // why n-1 relaxations bcz longest path is linked list graph 
+    // whose max length is n-1
+    // eg ->     1 -> 2 -> 3 -> 4 -> 5 (n = 5 , m = 5-1) ;
+    
+	    for(int i=1; i<n; i++)
+	    {
+	        for(auto &edge : edges)
+	        {
+	            int from = edge[0] , to = edge[1] , wt = edge[2] ;
+	            if(dist[from] + wt < dist[to])
+	            {
+	                 dist[to] = dist[from] + wt ;
+	            }
+	        }
+	    }
+	    
+        for(auto &edge : edges)
+        {
+            int from = edge[0] , to = edge[1] , wt = edge[2] ;
+            if(dist[from] + wt < dist[to])
+            {
+                 return 1 ;
+            }
+        }
+	    return 0 ;
+	}
